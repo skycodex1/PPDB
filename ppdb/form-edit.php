c03a8a2391995c2e1ecd3b8825162fc5829aa204
skycodex1 <147239679+skycodex1@users.noreply.github.com>
2024-02-28 21:50:23 +0700

    Update form-edit.php

--- ppdb/form-edit.php
+++ ppdb/form-edit.php
@@ -2,20 +2,16 @@
 
 include("config.php");
 
-// kalau tidak ada id di query string
 if( !isset($_GET['id']) ){
     header('Location: list-siswa.php');
 }
 
-//ambil id dari query string
 $id = $_GET['id'];
 
-// buat query untuk ambil data dari database
 $sql = "SELECT * FROM calon_siswa WHERE id=$id";
 $query = mysqli_query($db, $sql);
 $siswa = mysqli_fetch_assoc($query);
 
-// jika data yang di-edit tidak ditemukan
 if( mysqli_num_rows($query) < 1 ){
     die("data tidak ditemukan...");
 }
